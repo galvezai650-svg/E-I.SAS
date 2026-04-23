82c8dd2e3d2a646691412a51581f52c274d39e8f
Z User <z@container>
2026-04-23 20:25:49 +0000

    29eb7694-7253-44bf-a1a8-9a0d3611d61c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -865,7 +865,7 @@ function GalleryCarousel() {
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-brand-light to-brand-lighter">Proyectos</span>
           </h2>
           <p className="text-white/50 max-w-2xl mx-auto text-lg">
-            Registro fotográfico de nuestras obras en ejecución, que da fiel testimonio de nuestro irrestricto compromiso con la calidad.
+            Registro fotográfico de nuestras obra
           </p>
         </motion.div>
 
